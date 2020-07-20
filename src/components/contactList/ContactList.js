b3e6477a7cd44b61076cbda7af9e679d59394508
William Baker <wbaker@gmail.com>
2020-07-20 13:19:44 -0400

    contact display with dummy data

--- src/components/contactList/ContactList.js
+++ src/components/contactList/ContactList.js
@@ -3,16 +3,53 @@ import React from 'react';
 import FluidGrid from './presentation/FluidGrid';
 import ContactCard from './presentation/ContactCard';
 
+import { v4 as uuidv4 } from 'uuid';
+
+const dummyData = [
+  {
+    id: uuidv4(),
+    firstName: 'asdf',
+    lastName: 'zxcv',
+    email: 'zxcv@qwer.com',
+    phone: '123412341234',
+  },
+  {
+    id: uuidv4(),
+    firstName: 'asdf',
+    lastName: 'zxcv',
+    email: 'zxcv@qwer.com',
+    phone: '123412341234',
+  },
+  {
+    id: uuidv4(),
+    firstName: 'asdf',
+    lastName: 'zxcv',
+    email: 'zxcv@qwer.com',
+    phone: '123412341234',
+  },
+  {
+    id: uuidv4(),
+    firstName: 'asdf',
+    lastName: 'zxcv',
+    email: 'zxcv@qwer.com',
+    phone: '123412341234',
+  },
+  {
+    id: uuidv4(),
+    firstName: 'asdf',
+    lastName: 'zxcv',
+    email: 'zxcv@qwer.com',
+    phone: '123412341234',
+  },
+];
+
 const ContactList = () => {
   return (
     <FluidGrid>
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
+      {dummyData.map(c => <ContactCard
+        key={c.id}
+        { ...c }
+      />)}
     </FluidGrid>
   );
 }
--- src/components/contactList/presentation/ContactCard.js
+++ src/components/contactList/presentation/ContactCard.js
@@ -1,23 +1,41 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
 
-const ContactCard = () => {
-  return (
-    <div className="card">
-      <div className="card-content">
-        <div className="content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-          <a href='/'>@bulmaio</a>. <a href='/'>#css</a> <a href='/'>#responsive</a>
-          <br />
-          <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-        </div>
+const ContactCard = ({
+  id,
+  firstName,
+  lastName,
+  email,
+  phone, 
+}) => (
+  <div className="card">
+    <div className="card-content">
+      <div className="content">
+        <ul>
+          <li><strong>First Name:</strong> {firstName}</li>
+          <li><strong>Last Name:</strong> {lastName}</li>
+          <li><strong>Email:</strong> {email}</li>
+          <li><strong>Phone:</strong> {phone}</li>
+        </ul>
       </div>
-      <footer className="card-footer">
-        <a href='/' className="card-footer-item">Save</a>
-        <a href='/' className="card-footer-item">Edit</a>
-        <a href='/' className="card-footer-item">Delete</a>
-      </footer>
     </div>
-  );
-};
+    <footer className="card-footer">
+      <button className="card-footer-item button is-info is-light">
+        <span className="icon">
+          <FontAwesomeIcon icon={faEdit} />
+        </span>
+        <span>Edit</span>
+      </button>
+      <button className="card-footer-item button is-danger is-light">
+        <span className="icon">
+          <FontAwesomeIcon icon={faTrash} />
+        </span>
+        <span>Delete</span>
+      </button>
+    </footer>
+  </div>
+);
 
 export default ContactCard;
