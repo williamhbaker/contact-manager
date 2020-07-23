69d25e7e482e8443de05321227664282d6b48d32
William Baker <wbaker@gmail.com>
2020-07-23 14:57:48 -0400

    nav bar reformatting

--- src/features/settings/Settings.js
+++ src/features/settings/Settings.js
@@ -37,7 +37,7 @@ const Settings = () => {
       Object.assign({}, field, { value: newVal })
     );
     setFields(newFields);
-    api.setDelay(Number(newVal));
+    thisName === 'delay' && api.setDelay(Number(newVal));
   };
 
   const handleResetClick = () => {
